774807657c8fab9a721c2572ecff521a9f4ab229
Ahmed AlSaeed <36594915+aalsaeed@users.noreply.github.com>
2026-08-17 23:19:07 +0300

    feat(integration): add inbox acceptance use case

---
+++ fleetops-integration/src/main/java/com/aalsaeed/fleetops/integration/application/port/in/AcceptErpShipmentDeliveryUseCase.java
@@ -0,0 +1,6 @@
+package com.aalsaeed.fleetops.integration.application.port.in;
+
+public interface AcceptErpShipmentDeliveryUseCase {
+
+    AcceptErpShipmentDeliveryResult accept(AcceptErpShipmentDeliveryCommand command);
+}
