ae30c34ea854c74199cd0ef21bdb2c76b52a6e20
Ahmed AlSaeed <36594915+aalsaeed@users.noreply.github.com>
2026-08-24 00:01:44 +0300

    feat(concurrency): map vehicle conflicts to HTTP 409

--- fleetops-vehicle/src/main/java/com/aalsaeed/fleetops/vehicle/api/rest/VehicleRestExceptionHandler.java
+++ fleetops-vehicle/src/main/java/com/aalsaeed/fleetops/vehicle/api/rest/VehicleRestExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.aalsaeed.fleetops.vehicle.api.rest;
 
+import com.aalsaeed.fleetops.common.concurrency.OptimisticConcurrencyConflictException;
 import com.aalsaeed.fleetops.vehicle.application.exception.VehicleAlreadyExistsException;
 import com.aalsaeed.fleetops.vehicle.application.exception.VehicleNotFoundException;
 import com.aalsaeed.fleetops.vehicle.domain.InvalidVehicleStatusTransitionException;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-@RestControllerAdvice
+@RestControllerAdvice(assignableTypes = VehicleController.class)
 public class VehicleRestExceptionHandler {
 
     @ExceptionHandler(VehicleNotFoundException.class)
@@ -28,6 +29,17 @@ public class VehicleRestExceptionHandler {
     @ExceptionHandler(InvalidVehicleStatusTransitionException.class)
     ProblemDetail handleInvalidStatusTransition(InvalidVehicleStatusTransitionException exception) {
         return createProblem(HttpStatus.CONFLICT, "INVALID_VEHICLE_STATUS_TRANSITION", exception.getMessage());
+    }
+
+    @ExceptionHandler(OptimisticConcurrencyConflictException.class)
+    ProblemDetail handleConcurrencyConflict(OptimisticConcurrencyConflictException exception) {
+        ProblemDetail detail = createProblem(
+                HttpStatus.CONFLICT,
+                "OPTIMISTIC_CONCURRENCY_CONFLICT",
+                exception.getMessage());
+        detail.setProperty("resourceType", exception.resourceType());
+        detail.setProperty("resourceId", exception.resourceId());
+        return detail;
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
